5ef982e3df62aa449d6be55934e8a325be519de2
Tofomb <tobias.c.forsberg@gmail.com>
2018-11-22 11:01:20 +0100

    created torq.mvc

---
+++ Torq/Torq.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Torq.MVC.Startup))]
+namespace Torq.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
