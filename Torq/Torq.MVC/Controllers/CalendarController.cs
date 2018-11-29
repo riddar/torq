476789d8d38ce2cb71fc96ce3cf873d57df7df71
Tofomb <tobias.c.forsberg@gmail.com>
2018-11-29 13:52:22 +0100

    added createSchedule view

--- Torq/Torq.MVC/Controllers/CalendarController.cs
+++ Torq/Torq.MVC/Controllers/CalendarController.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Torq.Models.Objects;
+using Torq.MVC.EmployeesService;
+using Torq.MVC.SchedualsService;
 
 namespace Torq.MVC.Controllers
 {
     public class CalendarController : Controller
     {
+        
         // GET: Calendar
         public ActionResult Index()
         {
@@ -28,11 +31,39 @@ namespace Torq.MVC.Controllers
             MockList.Add(new Schedule { Id = 4, ClockedIn = true, Employee = new Employee { Id = 1, FirstName = "Tobb", LastName = "Fobbs", IsOnline = false }, EndTime = new DateTime(2018, 5, 4, 18, 30, 52), StartTime = new DateTime(2018, 5, 4, 8, 30, 52), Salary = null });
             MockList.Add(new Schedule { Id = 5, ClockedIn = false, Employee = new Employee { Id = 2, FirstName = "Mobbs", LastName = "Robbs", IsOnline = false }, EndTime = new DateTime(2018, 5, 5, 18, 30, 52), StartTime = new DateTime(2018, 5, 5, 8, 30, 52), Salary = null });
 
+            Schedule[] schedulesArray = new Schedule[] { };
+
+            using (ScheduleServiceClient db = new ScheduleServiceClient())
+            {
+                schedulesArray = db.GetSchedules();
+            }
+
+            var sortArray = schedulesArray.OrderBy(m => m.StartTime).ToArray();
             ViewBag.CalendarDataArray = MockArray;
             ViewBag.CalendarDataList = MockList;
             //@model Torq.Models.Objects.Schema[]
             //@model List<Torq.Models.Objects.Schema>
-            return View(MockArray);
+            return View(sortArray);
+        }
+
+        public ActionResult CreateSchedule()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateSchedule([Bind(Include = "StartTime,EndTime")] Schedule schedule, int? Id)
+        {
+            schedule.ClockedIn = false;
+            var x = schedule;
+            
+            using (ScheduleServiceClient db = new ScheduleServiceClient())
+            {
+                db.CreateSchedule(schedule);
+            }
+
+                return Redirect("~/calendar/index");
         }
     }
 }
