970602548b58826fd8dd59bace5d9c95d1c44d01
Oscar Wolter <Oscardawolter@gmail.com>
2018-12-04 10:33:21 +0100

    Added stuff for webapp

---
+++ Torq/Torq.MVC/Controllers/CalendarController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Torq.MVC.Controllers
+{
+    public static class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        
+    }
+}
---
+++ Torq/Torq.MVC/MethodLiberary/DateHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Torq.MVC.MethodLiberary
+{
+    public static class DateHandler
+    {
+        public static DayOfWeek GetDayOfWeek(int day)
+        {
+            DateTime dateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, day);
+            return dateTime.DayOfWeek;
+        }
+    }
+}
