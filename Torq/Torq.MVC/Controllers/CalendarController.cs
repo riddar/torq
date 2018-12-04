8b5f48a4c0685bf4c76fadec965bc525a29ee18b
Oscar Wolter <Oscardawolter@gmail.com>
2018-12-04 14:43:37 +0100

    Worked on Calendar view

--- Torq/Torq.MVC/Controllers/CalendarController.cs
+++ Torq/Torq.MVC/Controllers/CalendarController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Torq.MethodLiberary;
 
 namespace Torq.MVC.Controllers
 {
-    public static class CalendarController : Controller
+    public  class CalendarController : Controller
     {
+        DateHandler dateHandler = new DateHandler();
+
         // GET: Calendar
         public ActionResult Index()
         {
+            ViewBag.DayOfWeek = dateHandler.GetDayOfWeek(1);
             return View();
         }
 
